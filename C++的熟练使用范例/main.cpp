e8dec4481918e7f928a7350af437b4c3b0886c24
hansonboy <jianwei2013@126.com>
2016-04-07 12:59:54 +0800

    Initial Commit

---
+++ C++的熟练使用范例/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++的熟练使用范例
+//
+//  Created by wangjianwei on 16/4/7.
+//  Copyright © 2016年 JW. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
